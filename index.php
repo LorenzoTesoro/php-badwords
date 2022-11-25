25b7c0d078b5bae390dd8525251d6e4cf301f0b5
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-11-25 15:33:57 +0100

    initial commit: problem with printing words

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Php-Badwords</title>
+  </head>
+  <body>
+    <form action="response.php" method="get">
+      <textarea name="paragraph" id="" cols="30" rows="10"></textarea>
+      <input type="text" name="bad_word" id="bad_word" />
+      <button type="submit">Submit</button>
+    </form>
+  </body>
+</html>
